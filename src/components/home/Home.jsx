f7bcb2592983fb120b57a90c8f962b173255db55
Gabrielle <gabrielle.mi@hotmail.com>
2022-05-03 13:18:47 -0400

    feat: add skills layer

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import Btn from "../btn/Btn";
 import Header from "../header/Header";
 import Projects from "../projects/Projects";
 import React from "react";
+import Skills from "../skills/Skills";
 
 export default function Home() {
 	return (
@@ -12,6 +13,7 @@ export default function Home() {
 			</Header>
 			<main>
 				<About />
+				<Skills />
 				<Projects />
 			</main>
 		</>
